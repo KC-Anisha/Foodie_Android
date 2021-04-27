7928d4a411800adc7031c6b827806ca622ddc17a
Anisha KC <anisha.kc7219@gmail.com>
2021-04-27 11:01:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/foodie_android/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.foodie_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.yellow));
+        }
+    }
+}
